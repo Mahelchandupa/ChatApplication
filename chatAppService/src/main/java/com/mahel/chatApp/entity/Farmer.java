45393a30ff1e034bae7d475ce2e0bb2012627951
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-05-17 21:19:15 +0530

    farmer and user entity

---
+++ chatAppService/src/main/java/com/mahel/chatApp/entity/Farmer.java
@@ -0,0 +1,30 @@
+package com.mahel.chatApp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Farmer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Long phone;
+
+    private String address;
+
+    private String city;
+}
