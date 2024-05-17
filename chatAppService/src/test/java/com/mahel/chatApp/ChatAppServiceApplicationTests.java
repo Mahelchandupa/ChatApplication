eef9b36022801859660e8c3c1c3b5fa7bdb2193a
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-05-17 21:08:55 +0530

    first commit

---
+++ chatAppService/src/test/java/com/mahel/chatApp/ChatAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahel.chatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
