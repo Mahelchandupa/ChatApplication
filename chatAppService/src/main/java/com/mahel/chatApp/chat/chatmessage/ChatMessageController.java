9ade8e3f1028242d763fdd8a9f1290327c7566af
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-05-19 15:08:28 +0530

    implement chatMessage

---
+++ chatAppService/src/main/java/com/mahel/chatApp/chat/chatmessage/ChatMessageController.java
@@ -0,0 +1,39 @@
+package com.mahel.chatApp.chat.chatmessage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatMessageController {
+
+    @Autowired
+    private ChatMessageService chatMessageService;
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    public void processMessage(
+            @PathVariable ChatMessage chatMessage
+    ) {
+        ChatMessage savedMsg = chatMessageService.save(chatMessage);
+        simpMessagingTemplate.convertAndSendToUser(
+                chatMessage.getRecipientId(), "/queue/messages", null
+        );
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<List<ChatMessage>> findByChatMessage(
+            @PathVariable("senderId") String senderId,
+            @PathVariable("recipientId") String recipientId
+    ) {
+        return ResponseEntity.ok(chatMessageService.findChatMessages(senderId, recipientId));
+    }
+}
